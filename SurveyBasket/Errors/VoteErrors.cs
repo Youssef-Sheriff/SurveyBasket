51f96303a803431dcee9486a61ce82490f22201b
Youssef Sherif <sherify766@gmail.com>
2024-10-27 15:03:12 +0300

    Add Get Available endpoint to QuestionService this endpoint can reeceive the User id and poll id to check if user has voted to this poll before or not and check also if poll is exists or not if all condition ok i return the question to enable user vote,

---
+++ SurveyBasket/Errors/VoteErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyBasket.Errors;
+
+public static class VoteErrors
+{
+     public static readonly Error DupplicatedVote =
+        new("Vote.duplicated", "You have already voted for this poll.", StatusCodes.Status409Conflict);
+
+}
--- SurveyBasket/Services/QuestionService.cs
+++ SurveyBasket/Services/QuestionService.cs
@@ -30,6 +30,33 @@ public class QuestionService(ApplicationDbContext context) : IQuestionService
         return Result.Success<IEnumerable<QuestionResponse>>(questions);
     }
 
+    public async Task<Result<IEnumerable<QuestionResponse>>> GetAvailableAsync(int pollId, string userId, CancellationToken cancellationToken = default)
+    {
+        var hasVote = await _context.Votes.AnyAsync(x=>x.PollId == pollId && x.UserId == userId, cancellationToken);
+
+        if (hasVote)
+            return Result.Failure<IEnumerable<QuestionResponse>>(VoteErrors.DupplicatedVote);
+
+        var pollIsExists = await _context.Polls.AnyAsync(x => x.Id == pollId && x.IsPublished && x.StartsAt <= DateOnly.FromDateTime(DateTime.UtcNow) && x.EndsAt >= DateOnly.FromDateTime(DateTime.UtcNow), cancellationToken);
+
+        if (!pollIsExists)
+            return Result.Failure<IEnumerable<QuestionResponse>>(PollErrors.PollNotFound);
+
+        var questions = await _context.Questions
+            .Where(x => x.PollId == pollId && x.IsActive)
+            .Include(x => x.Answers)
+            .Select(q => new QuestionResponse(
+                q.Id,
+                q.Content,
+                q.Answers.Where(a => a.IsActive).Select(a => new AnswerResponse(a.Id, a.Content))
+                ))
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        return Result.Success<IEnumerable<QuestionResponse>>(questions);
+
+    }
+
     public async Task<Result<QuestionResponse>> GetAsync(int pollId, int id, CancellationToken cancellationToken = default)
     {
 
